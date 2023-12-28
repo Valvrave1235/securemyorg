d556d211bd85819fa862b3cbd7ecbdd5006f01e7
Valvrave1235 <vtbakun@gmail.com>
2023-12-28 14:03:01 +0800

    init

---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <div className="flex flex-col gap-4 bg-stone-950">
+        <h2>
+          We help companies <br />
+          scale while being secure
+        </h2>
+
+        <p>
+          Hi, we’re a cyber security and devops agency which helps fast paced
+          startups scale.
+        </p>
+        <button>
+        Book a Call
+        </button>
+      </div>
+      <p></p>
+    </main>
+  );
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
---
+++ src/pages/riswan.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const RiswanPage = () => {
+  return <main>asu</main>;
+};
+
+export default RiswanPage;
+
+export const Head = () => <title>Home Page</title>;
