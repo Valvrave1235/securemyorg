1e1963591ee306db48ebae6817d4d7b35ef41a08
Valvrave1235 <vtbakun@gmail.com>
2023-12-29 15:59:13 +0800

    baru

--- src/pages/index.js
+++ src/pages/index.js
@@ -212,7 +212,7 @@ const IndexPage = () => {
           questions and help us to get the most out of the <br />
           product.</p>
           </div>
-        
+          <dir></dir>
           <div>
            <div>
             <img src="../images/alexander.png" alt="A dinosaur" />
